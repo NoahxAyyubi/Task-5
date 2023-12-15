0e2bf5c0ed92e9bc3d291eb656f64838838f02a9
Noah Muhammad <noahxmuhammad@gmail.com>
2023-12-15 15:17:52 -0500

    add css layout to a list of names

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+  const names = ['Alice' , 'Bob', 'Charlie', 'David'];
+  const App = () => {
+  return (
+      <div className = "app-container">
+        <h2>List of Names:</h2>
+        <ul className="name-list">
+          {names.map((name, index) =>(<li key={index}>{index +1}. {name}</li>))}
+        </ul>
+      </div>
+  );
+};
+export default App
